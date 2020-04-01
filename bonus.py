5983dfde9fb7ae4ad8678a75de57268f0578a79e
Jonathan Cox <cox.jonathan@gmail.com>
2020-04-01 11:41:37 -0400

    added comments

--- bonus.py
+++ bonus.py
@@ -1,8 +1,11 @@
 #prompt user for a number in days
 days = input('Give me a number of days: ')
 
+#convert the given amount of days into a variable 'seconds'
+#dont forget to convert the users input into an integer
 seconds = int(days) * 24 * 60 * 60
 
+#return the amount of seconds in the given days
 print(f'There are {seconds} seconds in {days} day(s)')
 
 
