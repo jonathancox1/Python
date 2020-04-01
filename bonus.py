69a4ad53efd0d696e81d3d81a6bc1160169c3860
Jonathan Cox <cox.jonathan@gmail.com>
2020-04-01 11:51:48 -0400

    added if/else

--- bonus.py
+++ bonus.py
@@ -5,8 +5,17 @@ days = input('Give me a number of days: ')
 #dont forget to convert the users input into an integer
 seconds = int(days) * 24 * 60 * 60
 
-#return the amount of seconds in the given days
-print(f'There are {seconds} seconds in {days} day(s)')
+
+#determine if users input results in plural form of day/days
+day_s = ''
+if (int(days) > 1):
+    day_s = 'days'
+else:
+    day_s = 'day'
+
+
+#return the amount of seconds in the given number of day/days
+print(f'There are {seconds} seconds in {days} {day_s}')
 
 
 
